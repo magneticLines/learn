c86eb2872f2ba8fd63b0d3964a17f53f1aa0677e
shea <2497296034@qq.com>
2025-05-01 00:16:06 +0800

    添加spring boot 项目基本结构

---
+++ springbootDemo/src/test/java/com/learn/shea/SheaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.shea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
