84b1f318eabce9a2eaab4c2db9b7f65ecb1a4698
shea <2497296034@qq.com>
2025-09-13 23:39:12 +0800

    fix:添加java 并发测试

--- springbootDemo/src/main/java/com/learn/shea/controller/TestController.java
+++ springbootDemo/src/main/java/com/learn/shea/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.learn.shea.controller;
 
+import com.learn.shea.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Autowired
+    TestService testService;
+
     @GetMapping("/hello")
     public String helloWorld(){
         return "hello world";
+    }
+    @GetMapping("/test")
+    public int test(){
+        try {
+            return testService.test();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
 }
